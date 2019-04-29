189e161894213ed2c00897efc5132860538a8ffe
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-04-28 19:21:16 -0600

    Add solution for analyze_text assignment

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,9 @@
-# Remember to reach out for help after your own due diligence
-
 def analyze_text(text):
-    # Your code here
+    alphas_num = 0
+    for index in range(len(text)):
+        if text[index].isalpha():
+            alphas_num = alphas_num + 1
+
+    count_e = text.count('e') + text.count('E')
+    percent = count_e / alphas_num * 100
+    print(f'The text contains {alphas_num} alphabetic characters, of which {count_e} ({percent}%) are "e".')
